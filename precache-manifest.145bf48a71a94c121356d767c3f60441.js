80a36e829c68fc63df2b6f89012c47920c246a68
Artur Radomyski <widzepsaprzeddrzwiami@gmail.com>
2019-02-09 23:32:59 +0100

    Updates

---
+++ precache-manifest.145bf48a71a94c121356d767c3f60441.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "ada2298c093d44c5a56e",
+    "url": "/github-search/static/js/runtime~main.ada2298c.js"
+  },
+  {
+    "revision": "6afd856c34c6d061b359",
+    "url": "/github-search/static/js/main.6afd856c.chunk.js"
+  },
+  {
+    "revision": "42ce2ab0baf7569b5673",
+    "url": "/github-search/static/js/1.42ce2ab0.chunk.js"
+  },
+  {
+    "revision": "ded6c9cb6b1efdb03463047ba37ac57a",
+    "url": "/github-search/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.dd3633f050ab40921d74ee59a43152ce.js"
+  "/github-search/precache-manifest.145bf48a71a94c121356d767c3f60441.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/github-search/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
